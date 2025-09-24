98de5f857adda1abc7ff16660747e2b294aad008
creativeDevHealer <code.healer.1120@gmail.com>
2025-09-23 19:55:13 -0700

    firebase configured

--- callControl.js
+++ callControl.js
@@ -129,8 +129,7 @@ const webhookController = async (req, res) => {
           try {
             const callData = await firebaseService.getCallData(callControlId);
             if (callData && callData.phoneNumber) {
-              const vmCallData = await firebaseService.getCallData(callControlId);
-              const smsResult = await sendSMS(callData.phoneNumber, vmCallData.script);
+              const smsResult = await sendSMS(callData.phoneNumber, callData.script);
               // console.log(vmCallData.script);
               
               if (smsResult.success) {
